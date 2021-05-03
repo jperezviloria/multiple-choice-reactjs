448a910d269b006160d538916a9add9b50f18673
jperezviloria <protobot.dev@gmail.com>
2021-05-02 23:25:20 -0300

    adding switch toggle and adding two botton on Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,9 @@ const Header = () =>{
     return(
         <div className="header-style">
             <a className="header-style-link" href="">Question Factory</a>
+            <a className="header-style-link" href="">Show Questions</a>
             <a className="header-style-link" href="">Test type Factory</a>
+            <a className="header-style-link" href="">Show Test type</a>
         </div>
     )
 }
--- src/pages/questionFactory/QuestionFactory.jsx
+++ src/pages/questionFactory/QuestionFactory.jsx
@@ -1,40 +1,151 @@
-import React from "react"
+import React,{useState} from "react"
+import {useForm} from "react-hook-form"
+import axios from "axios"
+import Swal from "sweetalert2"
+import Switch from "react-switch";
+
 import "./QuestionFactory.css"
 
+
 const QuestionFactory = () =>{
+
+    const {register, handleSubmit} = useForm();
+    const [checkedOne, setCheckedOne] = useState(false)
+    const [checkedTwo, setCheckedTwo] = useState(false)
+    const [checkedThree, setCheckedThree] = useState(false)
+    const [checkedFour, setCheckedFour] = useState(false)
+    //const [handleChange, setHandleChange] = useState(handleSubmit.bind)
+
+    const onSubmitUserForm = (data) =>{
+
+            const arrayBooleans = [checkedOne, checkedTwo, checkedThree, checkedFour]
+            const result = checkThatHaveOnlyOneTrue(arrayBooleans)
+            console.log(result)
+
+            const body ={
+            idLevel: data.level,
+            question: data.question,
+            answers: data
+        }
+        printBody(body)
+    }
+
+    const printBody = (body) =>{
+        console.log(body)
+    }
+
+    const changeStateSlice = (booleanValue) =>{
+        return booleanValue === true ?  false : true
+    }
+
+    const checkThatHaveOnlyOneTrue = (booleanArray) =>{
+        const result = booleanArray.filter(condition => condition === true)
+
+        if (result.length > 1 || result.length === 0)
+        return false
+        else if (result.length === 1)
+        return true
+        
+    }
+
+
     return (
-        <div>
+        <form onSubmit={handleSubmit(onSubmitUserForm)}>
             <div>
                 <p>Ingresa la pregunta</p>
-                <input type="" placeholder="question"/>
+                <input 
+                type="" 
+                placeholder="question"
+                {...register("question")}/>
             </div>
             <div>
-                <select name="" id="">
+                <select 
+                name="" 
+                id=""
+                {...register("level")}>
                     <option value="">seleccione ...</option>
-                    <option value="">level 1</option>
-                    <option value="">level 2</option>
+                    <option value="1">level 1</option>
+                    <option value="2">level 2</option>
                 </select>
             </div>
             <div>
                 <div>
-                <input type="text" placeholder="answer 1"/>
-                <input type="checkbox"/>
+                <input 
+                type="text" 
+                placeholder="answer 1"
+                {...register("answer1")}/>
+                <Switch
+                onChange={(e) => setCheckedOne(changeStateSlice(checkedOne))}  
+                checked={checkedOne}
+                onColor="#86d3ff"
+                onHandleColor="#2693e6"
+                handleDiameter={15}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                height={20}
+                width={48}/>
                 </div>
                 <div>
-                <input type="text" placeholder="answer 2"/>
-                <input type="checkbox"/>
-                </div>
-                <div>
-                <input type="text" placeholder="answer 3"/>
-                <input type="checkbox"/>
-                </div>
-                <div>
-                <input type="text" placeholder="answer 4"/>
-                <input type="checkbox"/>
-                </div>
+                <input 
+                type="text" 
+                placeholder="answer 2"
+                {...register("answer2")}/>
+                <Switch
+                {...register("check2")}
+                onChange={(e) => setCheckedTwo(changeStateSlice(checkedTwo))} 
+                checked={checkedTwo}
+                onColor="#86d3ff"
+                onHandleColor="#2693e6"
+                handleDiameter={15}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                height={20}
+                width={48}
+                />
+                </div><div>
+                <input 
+                type="text" 
+                placeholder="answer 3"
+                {...register("answer3")}/>
+                <Switch 
+                {...register("check3")}
+                onChange={(e) => setCheckedThree(changeStateSlice(checkedThree))} 
+                checked={checkedThree}
+                onColor="#86d3ff"
+                onHandleColor="#2693e6"
+                handleDiameter={15}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                height={20}
+                width={48}/>
+                </div><div>
+                <input 
+                type="text" 
+                placeholder="answer 4"
+                {...register("answer4")}/>
+                <Switch
+                {...register("check4")}
+                onChange={(e) => setCheckedFour(changeStateSlice(checkedFour))}  
+                checked={checkedFour}
+                onColor="#86d3ff"
+                onHandleColor="#2693e6"
+                handleDiameter={15}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                height={20}
+                width={48}/>
+                </div>  
             </div>
-            <button>enviar</button>
-        </div>
+            <button type="submit">enviar</button>
+        </form>
     )
 }
 
